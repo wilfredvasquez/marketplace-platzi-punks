9536f0c0ce57b0d2cb066e678270b40558b95d16
Wilfred Vásquez <wilfredvas@gmail.com>
2021-11-26 20:00:15 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router';
+import Home from './views/home';
+import Punks from './views/punks';
+import Punk from './views/punk';
+import MainLayout from './layouts/main';
+
+function App() {
+  return (
+    <MainLayout>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/punks" exact element={<Punks />} />
+        <Route path="/punks/:tokenId" exact element={<Punk />} />
+      </Routes>
+    </MainLayout>
+  );
+}
+
+export default App;
---
+++ src/hooks/usePlatziPunksData/index.js
@@ -0,0 +1,147 @@
+import { useWeb3React } from "@web3-react/core";
+import { useCallback, useEffect, useState } from "react";
+import usePlatziPunks from "../usePlatziPunks";
+
+const getPunkData = async ({ platziPunks, tokenId }) => {
+  const [
+    tokenURI,
+    dna,
+    owner,
+  ] = await Promise.all([
+    platziPunks.methods.tokenURI(tokenId).call(),
+    platziPunks.methods.tokenDNA(tokenId).call(),
+    platziPunks.methods.ownerOf(tokenId).call(),
+  ]);
+
+  const [
+    accessoriesType,
+    clotheColor,
+    clotheType,
+    eyeType,
+    eyeBrowType,
+    facialHairColor,
+    facialHairType,
+    hairColor,
+    hatColor,
+    graphicType,
+    mouthType,
+    skinColor,
+    topType,
+  ] = await Promise.all([
+    platziPunks.methods.getAccessoriesType(dna).call(),
+    platziPunks.methods.getClotheColor(dna).call(),
+    platziPunks.methods.getClotheType(dna).call(),
+    platziPunks.methods.getEyeType(dna).call(),
+    platziPunks.methods.getEyeBrowType(dna).call(),
+    platziPunks.methods.getFacialHairColor(dna).call(),
+    platziPunks.methods.getFacialHairType(dna).call(),
+    platziPunks.methods.getHairColor(dna).call(),
+    platziPunks.methods.getHatColor(dna).call(),
+    platziPunks.methods.getGraphicType(dna).call(),
+    platziPunks.methods.getMouthType(dna).call(),
+    platziPunks.methods.getSkinColor(dna).call(),
+    platziPunks.methods.getTopType(dna).call(),
+  ])
+
+  const responseMetadata = await fetch(tokenURI);
+  const metadata = await responseMetadata.json();
+
+  return {
+    tokenId,
+    attributes: {
+      accessoriesType,
+      clotheColor,
+      clotheType,
+      eyeType,
+      eyeBrowType,
+      facialHairColor,
+      facialHairType,
+      hairColor,
+      hatColor,
+      graphicType,
+      mouthType,
+      skinColor,
+      topType,
+    },
+    tokenURI,
+    dna,
+    owner,
+    ...metadata,
+  };
+};
+
+// Plural
+const usePlatziPunksData = ({ owner = null} = {}) => {
+  const [punks, setPunks] = useState([]);
+  const { library } = useWeb3React();
+  const [loading, setLoading] = useState(true);
+  const platziPunks = usePlatziPunks();
+
+  const update = useCallback(async () => {
+    if (platziPunks) {
+      setLoading(true);
+
+      let tokenIds;
+
+      if(!library.utils.isAddress(owner)) {
+        const totalSupply = await platziPunks.methods.totalSupply().call();
+        tokenIds = new Array(Number(totalSupply)).fill().map((_, index) => index);
+      } 
+      else {
+        const balanceOf = await platziPunks.methods.balanceOf(owner).call();
+        const tokenIdsOfOwner = new Array(Number(balanceOf)).fill().map((_, index) => platziPunks.methods.tokenOfOwnerByIndex(owner, index).call());
+        tokenIds = await Promise.all(tokenIdsOfOwner);
+      }
+
+      const punksPromise = tokenIds.map((tokenId) =>
+        getPunkData({ tokenId, platziPunks })
+      );
+
+      const punks = await Promise.all(punksPromise);
+
+      setPunks(punks);
+      setLoading(false);
+    }
+  }, [platziPunks, owner, library?.utils]);
+
+  useEffect(() => {
+    update();
+  }, [update]);
+
+  return {
+    loading,
+    punks,
+    update,
+  };
+};
+
+// Singular
+const usePlatziPunkData = (tokenId = null) => {
+  const [punk, setPunk] = useState({});
+  const [loading, setLoading] = useState(true);
+  const platziPunks = usePlatziPunks();
+
+
+  const update = useCallback(async () => {
+    if (platziPunks && tokenId != null) {
+      setLoading(true);
+
+      const punkId = await getPunkData({ tokenId, platziPunks })
+
+      setPunk(punkId);
+      setLoading(false);
+    }
+  }, [platziPunks, tokenId]);
+
+  useEffect(() => {
+    update();
+  }, [update]);
+
+  return {
+    loading,
+    punk,
+    update,
+  };
+}
+
+export { usePlatziPunksData, usePlatziPunkData };
